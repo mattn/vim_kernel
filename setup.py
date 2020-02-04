ca3163b6f1176cf20e5bafc542ba0934c82f339a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-02-04 13:48:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vim_kernel",
-    version = "0.1",
+    version = "0.2",
     packages=find_packages(),
     description="SQLite3 Jupyter Kernel",
     url = "https://github.com/mattn/vim_kernel",
